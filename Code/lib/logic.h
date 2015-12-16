a665fa84e796f67efea9de6ffd93c194417556b3
Sam Fok <sambfok@gmail.com>
2015-12-16 13:45:23 -0800

    precise exceptions example

--- Code/lib/logic.h
+++ Code/lib/logic.h
@@ -1,2 +1,2 @@
-export template <pint N> defproc AND (bool i[N], o);
-export template <pint N> defproc OR (bool i[N], o);
+export template <pint N> defproc AND (globals g; bool i[N], o);
+export template <pint N> defproc OR (globals g; bool i[N], o);
